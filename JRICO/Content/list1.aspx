dab7e72e06f0635247793f4b9f591bbe8f1a58bd
cathal.reddin@bt.com <cathal.reddin@bt.com>
2013-04-20 00:29:54 +0100

    Retrieve Data; Search/Sort; Hyperlinks; Logging;

---
+++ JRICO/Content/list1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="list1.aspx.cs" Inherits="JRICO.Content.list1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+     Create New | Search 
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem value="ContractReference">Contract Reference</asp:ListItem>
+            <asp:ListItem value="ContractTitle">Contract Title</asp:ListItem>
+            <asp:ListItem value="LinkedContractReference">Associated Ref</asp:ListItem>
+            <asp:ListItem value="RecordType">Record Type</asp:ListItem>
+            <asp:ListItem value="ContractSystemPriceList">System Price List</asp:ListItem>
+            <asp:ListItem value="ContactName">Name</asp:ListItem>
+            <asp:ListItem value="ContactEmail">Email</asp:ListItem>
+            <asp:ListItem value="ContactNo">Phone</asp:ListItem>
+            <asp:ListItem value="ContractStatus">Contract Status</asp:ListItem>
+            <asp:ListItem value="HospitalName">Hospital Name</asp:ListItem>
+        </asp:DropDownList> for
+        <asp:TextBox ID="TextSearch" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="Go" onclick="Button1_Click" />
+        <asp:GridView ID="GridView1" runat="server" OnSorting="SortRecords" 
+            AllowSorting="True" DataKeyNames="ContractID" onrowdatabound="GridView_RowDataBound" >
+            <Columns>
+                <asp:hyperlinkfield text="Details" datanavigateurlfields="ContractID" 
+         datanavigateurlformatstring="/Content/Details.aspx?Id={0}" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ JRICO/Content/list1.aspx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JRICO.Content
+{
+    public partial class list1 : System.Web.UI.Page
+    {
+        string _connStr = ConfigurationManager.ConnectionStrings["SQLConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindData("none", " ");
+                Writelog("User_accessed_List_Page", "cathal");
+            }
+        }
+
+        public void Writelog(string LogData, string LogUser) 
+        {
+            SqlConnection conn = new SqlConnection(_connStr);
+            try
+            {
+                conn.Open();
+                String MyString = @"INSERT INTO Log(LogData, LogUser) VALUES('"+LogData+"','"+LogUser+"')";
+                //String MyString = "INSERT INTO Log(LogData, LogUser, LogDate) VALUES( " + LogData + " ,1,1/1/1)";
+                SqlCommand MyCmd = new SqlCommand(MyString, conn);
+                MyCmd.ExecuteScalar();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+        }
+        protected void GridView_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            e.Row.Cells[1].Visible = false;
+            GridViewRow row = e.Row;
+            List<TableCell> cells = new List<TableCell>();
+            foreach (DataControlField column in GridView1.Columns)
+            {
+                //Getting first Column of the Gridview
+                TableCell cell = row.Cells[0];
+
+                //Remove that cell from the gridview
+                row.Cells.Remove(cell);
+
+                //Adding that cell as the last cell in the gridview
+                cells.Add(cell);
+            }
+            // Add cells
+            row.Cells.AddRange(cells.ToArray()); 
+            
+        }
+
+        private void BindData(string column, string textSearch)
+        {
+            GridView1.DataSource = this.GetData(column, textSearch);
+            GridView1.DataBind();         
+        }
+
+          
+        private DataTable GetData(string column, string textSearch)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                string sqlSP = "sp_getContractList";
+
+                using (SqlCommand cmd = new SqlCommand(sqlSP, conn))
+                {
+                    cmd.Parameters.Add(new SqlParameter("@column", column));
+                    cmd.Parameters.Add(new SqlParameter("@TextSearch", textSearch));
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataAdapter ad = new SqlDataAdapter(cmd))
+                    {
+                        ad.Fill(table);
+                    }
+                }
+            }
+
+            return table;
+        }
+
+        protected void SortRecords(object sender, GridViewSortEventArgs e)
+        {
+            string sortExpression = e.SortExpression;
+            string direction = string.Empty;
+            if (sortDirection == SortDirection.Ascending)
+            {
+                sortDirection = SortDirection.Descending;
+                direction = " DESC ";
+            }
+            else
+            {
+                sortDirection = SortDirection.Ascending;
+                direction = " ASC ";
+            }
+            DataTable table = this.GetData(DropDownList1.SelectedValue, TextSearch.Text);
+            table.DefaultView.Sort = sortExpression + direction;
+
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+
+            Writelog("User_Sorts_on_" + sortExpression + "_" + direction, "cathal");
+        }
+
+        public SortDirection sortDirection
+        {
+            get 
+            {
+                if (ViewState["SortDirection"] == null)
+                {
+                    ViewState["SortDirection"] = SortDirection.Ascending;
+                }
+                return (SortDirection)ViewState["SortDirection"];
+            }
+            set
+            {
+                ViewState["SortDirection"] = value;
+            }
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            BindData(DropDownList1.SelectedValue, TextSearch.Text);
+            Writelog("User_Searched_on_dropdown_" + DropDownList1.SelectedValue + "_for_Text_" + TextSearch.Text, "cathal");
+  
+        }
+
+
+    }
+}
